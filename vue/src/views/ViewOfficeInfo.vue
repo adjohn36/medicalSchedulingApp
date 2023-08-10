23cff91588c19091f252b94216d45e8d37bd82f8
sailaja biswal <sailaja.biswa;@gmail.com[D[D[D[D[D[D[D[D[D[l[F>
2023-08-10 19:30:38 -0400

    wip

---
+++ vue/src/views/ViewOfficeInfo.vue
@@ -0,0 +1,41 @@
+
+
+
+<template>
+  <div>
+        <h2>Office Details</h2>
+        <label id='facility-name'>{{facilityInfo.FacilityName}}</label>
+        <label id='doctor-name'>{{facilityInfo.doctorName}}</label>
+        <label id='address'>{{facilityInfo.Address}}</label>
+        <label id='opening-hours'>{{facilityInfo.OpenTime}}</label>
+        <label id='opening-days'>{{facilityInfo.OpenDays}}</label>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:'office-info', 
+    props:{
+      facilityId:Number
+    }
+       
+    ,
+    data(){
+      return{
+        facilityInfo:[]
+      }
+    },
+     created(){
+     this.facilityInfo =  
+            this.$store.state.DoctorFacility.filter(facility => facility.id === this.facilityId); 
+            alert(this.facilityId)  ;     
+    }
+
+    
+}
+</script>
+
+<style scoped>
+
+</style>
