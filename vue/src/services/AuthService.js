eee1098449ed1f643197b87fa2d3ccaa6f50b180
Jerry Khoutsavanh <jkhout11@gmail.com>
2023-08-16 22:48:00 -0400

    syntax errors

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -1,60 +1,71 @@
-
 import axios from 'axios';
-//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-export default {
 
+export default {
   login(user) {
-    return axios.post('/login', user)
+    return axios.post('/login', user);
   },
 
   register(user) {
-    return axios.post('/register', user)
-  },
-  getUserProfile(){
-    return axios.get('/profile')
-  },
-  getDoctorOfficeInfo(){
-    return axios.get('/doctors/view-doctor')
-  },
-  getOfficeDetail(officeId){
-    return axios.get(`/office-info/${officeId}`)
-  },
-  getOfficeList(){
-    return axios.get('/office-info/all')
-  },
-  updateDoctorProfile(profile){
-    return axios.post('/save-profile', profile)
-  },
-  updatePatientProfile(profile){
-    return axios.post('/save-profile', profile)
-  },
-  getDoctorSchedule(day){    
-    return axios.get(`/update-availability/schedule?dayOfTheWeek=${day}`)
-  },
-  getAllReviews(){
-    return axios.get('/reviews/all')
-  },
-  getReviewsByReviewedOffice(officeId)
-    return axios.get(`/reviews/all/${officeId}`)  
-  },
-  getListBookAppointments(){
-    return axios.get('/agenda/listbookappointments')
-  },
-  postBookAnAppointment(appointment){
-    return axios.post('/agenda/bookanappointment', appointment)
-  },
-  getDoctorList(){
-  return axios.get('/doctors/all')
-
-  },
-  getReviewById(reviewId){
-    return axios.get(`/reviews/${reviewId}`)
-  },
-  updateOffice(doctorFacilityInfo)
-    return axios.put(`/office-info/update/${doctorFacilityInfo.officeId}`, doctorFacilityInfo)
+    return axios.post('/register', user);
   },
 
-  getDoctorSchedule(doctorId){
-    return axios.get(`/update-availability/${doctorId}`)
+  getUserProfile() {
+    return axios.get('/profile');
+  },
+
+  getDoctorOfficeInfo() {
+    return axios.get('/doctors/view-doctor');
+  },
+
+  getOfficeDetail(officeId) {
+    return axios.get(`/office-info/${officeId}`);
+  },
+
+  getOfficeList() {
+    return axios.get('/office-info/all');
+  },
+
+  updateDoctorProfile(profile) {
+    return axios.post('/save-profile', profile);
+  },
+
+  updatePatientProfile(profile) {
+    return axios.post('/save-profile', profile);
+  },
+
+  getDoctorScheduleByDay(day) {    
+    return axios.get(`/update-availability/schedule?dayOfTheWeek=${day}`);
+  },
+
+  getAllReviews() {
+    return axios.get('/reviews/all');
+  },
+
+  getReviewsByReviewedOffice(officeId) {
+    return axios.get(`/reviews/all/${officeId}`);
+  },
+
+  getListBookAppointments() {
+    return axios.get('/agenda/listbookappointments');
+  },
+
+  postBookAnAppointment(appointment) {
+    return axios.post('/agenda/bookanappointment', appointment);
+  },
+
+  getDoctorList() {
+    return axios.get('/doctors/all');
+  },
+
+  getReviewById(reviewId) {
+    return axios.get(`/reviews/${reviewId}`);
+  },
+
+  updateOffice(doctorFacilityInfo) {
+    return axios.put(`/office-info/update/${doctorFacilityInfo.officeId}`, doctorFacilityInfo);
+  },
+
+  getDoctorSchedule(doctorId) {
+    return axios.get(`/update-availability/${doctorId}`);
   }
 }
