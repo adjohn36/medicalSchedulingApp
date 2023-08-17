b3a9c9906ce679fcede12997e5fcf5817e1d53d4
Jerry Khoutsavanh <jkhout11@gmail.com>
2023-08-17 15:27:59 -0400

    doctor profile

--- vue/src/components/ManageDoctorProfile.vue
+++ vue/src/components/ManageDoctorProfile.vue
@@ -1,49 +1,92 @@
 <template>
-  <div>
-    <form id="profile-form" @submit.prevent="saveDoctorProfile">
-      <h1>Create Doctor Profile</h1>
-      <div class="form-element">
-        <label for="firstName">First Name:</label>
-        <input
-          id="firstName"
-          type="text"
-          v-model="doctor.doctorProfile.doctorFirstName"
-        />
-      </div>
-      <div class="form-element">
-        <label for="lastName">Last Name:</label>
-        <input
-          id="lastName"
-          type="text"
-          v-model="doctor.doctorProfile.doctorLastName"
-        />
-      </div>
-      <div class="form-element">
-        <label for="email">Email Address:</label>
-        <input
-          id="email"
-          type="email"
-          v-model="doctor.doctorProfile.doctorEmail"
-        />
-      </div>
-      <div class="form-element">
-        <label for="facility-input">Facility:</label>
-        <select id="facility-input" v-model="facility">
-          <option
-            v-for="office in officeList"
-            v-bind:key="office.id"
-            v-bind:value="office.id"
+  <div class="container">
+    <div class="header">
+      <nav>
+        <div class="nav-content">
+          <div class="logo">
+            <a href="#">
+              <img src="../img/NavBarLogo.png" alt="Logo" />
+            </a>
+          </div>
+          <div class="welcome-title">
+            <h1></h1>
+          </div>
+          <ul class="nav-links">
+            <li><router-link to="/doctor-portal">My Agenda</router-link></li>
+            <li>
+              <router-link to="/schedule-availability"
+                >Update My Availability</router-link
+              >
+            </li>
+            <li>
+              <router-link to="/update-office-info"
+                >Update Office Info</router-link
+              >
+            </li>
+            <li>
+              <router-link v-bind:to="{ name: 'doctor-profile' }"
+                >My Doctor Profile</router-link
+              >
+            </li>
+            <li><router-link to="/reviews">My Reviews</router-link></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="form-container">
+      <form id="profile-form" @submit.prevent="saveDoctorProfile">
+        <h1 class="page-title">Create Doctor Profile</h1>
+        <div class="form-element">
+          <label for="firstName">First Name:</label>
+          <input
+            id="firstName"
+            type="text"
+            class="form-input"
+            v-model="doctor.doctorProfile.doctorFirstName"
+          />
+        </div>
+        <div class="form-element">
+          <label for="lastName">Last Name:</label>
+          <input
+            id="lastName"
+            type="text"
+            class="form-input"
+            v-model="doctor.doctorProfile.doctorLastName"
+          />
+        </div>
+        <div class="form-element">
+          <label for="email">Email Address:</label>
+          <input
+            id="email"
+            type="email"
+            class="form-input"
+            v-model="doctor.doctorProfile.doctorEmail"
+          />
+        </div>
+        <div class="form-element">
+          <label for="facility-input">Facility:</label>
+          <select
+            id="facility-input"
+            class="form-input"
+            v-model="facility"
           >
-            {{ office.officeName }}
-          </option>
-        </select>
-      </div>
-      <div class="row button">
-        <button type="submit">Save</button>
-      </div>
-    </form>
+            <option
+              v-for="office in officeList"
+              :key="office.id"
+              :value="office.id"
+            >
+              {{ office.officeName }}
+            </option>
+          </select>
+        </div>
+        <div class="form-element button">
+          <button class="submit-button" type="submit">Save</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
+
 
 <script>
 import authService from "../services/AuthService";
@@ -134,6 +177,97 @@ export default {
   align-items: center;
   margin-bottom: 20px;
 }
+.button {
+  text-align: center;
+}
+.form-container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.page-title {
+  font-size: 24px;
+  color: #4070f4;
+  margin-bottom: 20px;
+}
+
+.form-input-group {
+  margin-bottom: 20px;
+}
+
+.form-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.submit-button {
+  background-color: #4070f4;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.submit-button:hover {
+  background-color: #3358c9;
+}
+
+.nav-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+}
+
+.logo img {
+  display: block;
+}
+
+.welcome-title {
+  color: #0d1fe0;
+}
+
+.nav-links {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+
+.nav-links li {
+  margin: 0 8px;
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: #0e2431;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 10px 4px;
+  transition: all 0.3s ease;
+}
+
+.nav-links a:hover {
+  color: #4070f4;
+}
+
+.form-element {
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.form-element label {
+  margin-right: 10px;
+}
+
 .button {
   text-align: center;
 }
--- vue/src/components/UpdateOfficeInfo.vue
+++ vue/src/components/UpdateOfficeInfo.vue
@@ -1,61 +1,117 @@
 <template>
-  <div id="updateOfficeInfo" class="text-center">
-    <form v-on:submit.prevent="updateOffice">
+  <div class="container">
+    <div class="header">
+      <nav v-show="isDoctor">
+        <div class="nav-content">
+          <div class="logo">
+            <a href="#">
+              <img src="../img/NavBarLogo.png" alt="Logo" />
+            </a>
+          </div>
+          <div class="welcome-title">
+            <h1></h1>
+          </div>
+          <ul class="nav-links">
+            <li><router-link to="/doctor-portal">My Agenda</router-link></li>
+            <li>
+              <router-link to="/schedule-availability"
+                >Update My Availability</router-link
+              >
+            </li>
+            <li>
+              <router-link to="/update-office-info"
+                >Update Office Info</router-link
+              >
+            </li>
+            <li>
+              <router-link v-bind:to="{ name: 'doctor-profile' }"
+                >My Doctor Profile</router-link
+              >
+            </li>
+            <li><router-link to="/reviews">My Reviews</router-link></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <form v-on:submit.prevent="updateOffice" class="office-form">
       <h1>Update Office Information</h1>
-     <!-- <div role="alert" v-if="updateOfficeErrors">
-        {{ updateOfficeErrorMsg }}
-      </div> -->
-      <office-info v-bind:doctorFacilityInfo="doctorFacilityInfo"/>
-    <div class="form-input-group">
-           <label for="officeName">Facility Name:</label>
-           <input type="text" id="officeName" v-model="doctorFacilityInfo.officeName"/>
-       </div>
+      <office-info v-bind:doctorFacilityInfo="doctorFacilityInfo" />
+      <div class="form-input-group">
+        <label for="officeName">Facility Name:</label>
+        <input
+          type="text"
+          id="officeName"
+          v-model="doctorFacilityInfo.officeName"
+        />
+      </div>
 
-
-    <div class="form-input-group">
+      <div class="form-input-group">
         <label for="streetAddress">Street:</label>
-        <input type="text" id="streetAddress" v-model="doctorFacilityInfo.streetAddress"/>
+        <input
+          type="text"
+          id="streetAddress"
+          v-model="doctorFacilityInfo.streetAddress"
+        />
       </div>
 
       <div class="form-input-group">
         <label for="city">City:</label>
-        <input type="text" id="city" v-model="doctorFacilityInfo.city"/>
+        <input type="text" id="city" v-model="doctorFacilityInfo.city" />
       </div>
 
       <div class="form-input-group">
         <label for="state">State:</label>
-        <input type="text" id="state" v-model="doctorFacilityInfo.state"/>
+        <input type="text" id="state" v-model="doctorFacilityInfo.state" />
       </div>
 
-       <div class="form-input-group">
+      <div class="form-input-group">
         <label for="zipCode">Zip:</label>
-        <input type="text" id="zipCode" v-model="doctorFacilityInfo.zipCode" placeholder ="ex: 19702" />
+        <input
+          type="text"
+          id="zipCode"
+          v-model="doctorFacilityInfo.zipCode"
+          placeholder="ex: 19702"
+        />
       </div>
 
-       <div class="form-input-group">
-           <label for="phone">Phone Number:</label>
-           <input type="text" id="phone" v-model="doctorFacilityInfo.phone" placeholder ="ex: (302) 999 - 9999"/>
-       </div>
-
       <div class="form-input-group">
-           <label for="openTime">Start Time:</label>
-           <input type="time" id="openTime" v-model="doctorFacilityInfo.openTime" />
-       </div>
-
-       <div class="form-input-group">
-           <label for="closeTime">Close Time:</label>
-           <input type="time" id="closeTime" v-model="doctorFacilityInfo.closeTime"/>
-       </div>
-      <div class="form-input-group">
-           <label for="openDays">Days Open:</label>
-           <input type="text" id="openDays" v-model="doctorFacilityInfo.openDays" placeholder ="Monday - Friday"/>
+        <label for="phone">Phone Number:</label>
+        <input
+          type="text"
+          id="phone"
+          v-model="doctorFacilityInfo.phone"
+          placeholder="ex: (302) 999 - 9999"
+        />
       </div>
-    
-      
-       
+
+      <div class="form-input-group">
+        <label for="openTime">Start Time:</label>
+        <input
+          type="time"
+          id="openTime"
+          v-model="doctorFacilityInfo.openTime"
+        />
+      </div>
+
+      <div class="form-input-group">
+        <label for="closeTime">Close Time:</label>
+        <input
+          type="time"
+          id="closeTime"
+          v-model="doctorFacilityInfo.closeTime"
+        />
+      </div>
+      <div class="form-input-group">
+        <label for="openDays">Days Open:</label>
+        <input
+          type="text"
+          id="openDays"
+          v-model="doctorFacilityInfo.openDays"
+          placeholder="Monday - Friday"
+        />
+      </div>
 
       <button type="submit">Update Information</button>
-     
     </form>
   </div>
 </template>
@@ -65,65 +121,147 @@ import authService from "../services/AuthService";
 // import OfficeInfo from "../components/OfficeInfo.vue";
 
 export default {
-  name: 'update-office-information',
+  name: "update-office-information",
   // components: {OfficeInfo},
   data() {
     return {
-      
       doctorFacilityInfo: {
-          id: 0,
-          officeName:'',
-          streetAddress: '',
-    city: '',
-    state: '',
-    zipCode: '',
-    phone: '',
-    openTime: '',
-    closeTime: '',
-    openDays: ''
-      }
-    }
+        id: 0,
+        officeName: "",
+        streetAddress: "",
+        city: "",
+        state: "",
+        zipCode: "",
+        phone: "",
+        openTime: "",
+        closeTime: "",
+        openDays: "",
+      },
+    };
   },
   methods: {
     // getDoctorFacilityInfo, call updateOffice method
-  getDoctorFacilityInfo(officeId) {
-    
-
+    getDoctorFacilityInfo(officeId) {
       authService.getOfficeDetail(officeId).then((response) => {
         if (response.status == 200) {
           this.doctorFacilityInfo = response.data;
         }
       });
-    
-   },
-  updateOffice() {
-    alert("hello")
-   authService.updateOffice(this.doctorFacilityInfo).then((response) => {
-         if (response.status == 200) {
-           
-           this.doctorFacilityInfo.push();
-         }
-      
-   });
-}
-
+    },
+    updateOffice() {
+      alert("hello");
+      authService.updateOffice(this.doctorFacilityInfo).then((response) => {
+        if (response.status == 200) {
+          this.doctorFacilityInfo.push();
+        }
+      });
+    },
   },
   created() {
     this.getDoctorFacilityInfo(this.$route.params.id);
     //this.updateOffice();
-  }
-
-}
-  
+  },
+};
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+}
+.header {
+  background-color: #a1de81;
+  width: 100%;
+  padding: 0px 0;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+}
 .form-input-group {
   margin-bottom: 1rem;
 }
 label {
   margin-right: 0.5rem;
 }
-
-</style>
+.office-form {
+  background-color: #ffffff;
+  border: 1px solid #ccc;
+  padding: 20px;
+  width: 100%;
+  max-width: 800px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  margin-top: 20px;
 }
+
+.page-title {
+  font-size: 24px;
+  color: #4070f4;
+  margin-bottom: 20px;
+}
+
+.form-input-group {
+  margin-bottom: 12px;
+}
+
+.form-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button[type="submit"] {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button[type="submit"]:hover {
+  background-color: #0056b3;
+}
+
+.nav-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+}
+
+.logo {
+  padding: 0;
+  margin: 0;
+}
+
+.logo img {
+  display: block;
+}
+
+.nav-links {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+
+.nav-links li {
+  margin: 0 8px;
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: #0e2431;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 10px 4px;
+  transition: all 0.3s ease;
+}
+
+.nav-links a:hover {
+  color: #4070f4;
+}
+</style>}
