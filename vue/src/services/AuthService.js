b25f8e00bb4516f26e5e58fad2fba0d7b2a28d4e
Adam Johnson <adamj00001@gmail.com>
2023-08-16 22:19:09 -0400

    updates

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -27,9 +27,11 @@ export default {
   getAllReviews(){
     return axios.get('/reviews/all')
   },
-  
   getReviewById(reviewId){
     return axios.get(`/reviews/${reviewId}`)
+  },
+  updateOffice(doctorFacilityInfo){
+    return axios.put(`/office-info/update/${doctorFacilityInfo.officeId}`, doctorFacilityInfo)
   }
   
 
